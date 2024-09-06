9fc0f6ae1b0b48568c4a3d787c244a7505214746
xiaoxiao <253451518@qq.com>
2024-09-06 10:25:47 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { h } from 'vue';
+//h函数
+const Btn = (props, ctx) => {
+  // const vnode = h('div',{class:'class1'},children子节点) vnode必须唯一
+  return h('div', {
+    class: 'class1 class2',
+    onClick: () => {
+      ctx.emit('on-click', 123)
+    }
+  }, ctx.slots.default())
+}
+const getNum = (num) => {
+  console.log(num);
+}
+</script>
+
+<template>
+  <div>这是</div>
+  <Btn text="是个按钮" @on-click="getNum">
+    <template #default>这是默认的插槽</template>
+  </Btn>
+</template>
